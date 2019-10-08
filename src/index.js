488f5735176051d27245667a38927ef48514ccc3
André Machado <andrels.machado82@gmail.com>
2019-10-08 20:54:40 -0300

    App mobile finalizado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
+import {YellowBox} from 'react-native';
 
 import Routes from './routes';
+
+YellowBox.ignoreWarnings(['Unrecognized WebSocket']);
 
 export default class App extends Component {
   render() {
