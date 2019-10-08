8fdfe23a8395118eca0a6ca8e93d3ed17bbf8cda
André Machado <andrels.machado82@gmail.com>
2019-10-08 13:45:24 -0300

    Configurando tela de Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   KeyboardAvoidingView,
@@ -9,9 +9,37 @@ import {
   StyleSheet,
 } from 'react-native';
 
+import AsyncStorage from '@react-native-community/async-storage';
+
+import api from '../services/api';
+
 import logo from '../assets/logo.png';
 
-export default function Login() {
+export default function Login({navigation}) {
+  const [email, setEmail] = useState('');
+  const [techs, setTechs] = useState('');
+
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user => {
+      if (user) {
+        navigation.navigate('List');
+      }
+    });
+  }, [navigation]);
+
+  async function handleSubmit() {
+    const response = await api.post('/sessions', {
+      email,
+    });
+
+    const {_id} = response.data;
+
+    await AsyncStorage.setItem('user', _id);
+    await AsyncStorage.setItem('techs', techs);
+
+    navigation.navigate('List');
+  }
+
   return (
     <KeyboardAvoidingView style={styles.container}>
       <Image source={logo} />
@@ -25,6 +53,8 @@ export default function Login() {
           keyboardType="email-address"
           autoCapitalize="none"
           autoCorrect={false}
+          value={email}
+          onChangeText={setEmail}
         />
 
         <Text style={styles.label}>TECNOLOGIAS *</Text>
@@ -34,9 +64,11 @@ export default function Login() {
           placeholderTextColor="#999"
           autoCapitalize="words"
           autoCorrect={false}
+          value={techs}
+          onChangeText={setTechs}
         />
 
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity onPress={handleSubmit} style={styles.button}>
           <Text style={styles.buttonText}>Encontrar spots</Text>
         </TouchableOpacity>
       </View>
